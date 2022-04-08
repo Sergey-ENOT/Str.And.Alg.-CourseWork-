da86cf5e23b5eaf8083d0e45ad78a15e9fa04023
Sergey-ENOT <sersh.razumov@mail.ru>
2022-04-08 18:03:36 +0600

    added data in csv

--- main.py
+++ main.py
@@ -13,24 +13,24 @@ ui.setupUi(MainWindow)
 MainWindow.show()
 
 
-# ds = Services()
-# ds.import_data("csv_files/services.csv")
-# for k in ds.dict_services.keys():
-#     print(k, ds.dict_services[k])
-# print("------------------------")
-# df = Tariffs()
-# df.import_data("csv_files/tariffs.csv")
-# for k in df.dict_tariffs.keys():
-#     for n in df.dict_tariffs[k].services:
-#         df.dict_tariffs[k].price += ds.dict_services[n].price
-#     print(k, df.dict_tariffs[k])
-# print("------------------------")
-# dc = Clients()
-# dc.import_data("csv_files/clients.csv")
-# for k in dc.dict_clients.keys():
-#     print(k, dc.dict_clients[k].name, dc.dict_clients[k].surname, dc.dict_clients[k].date.strftime("%d.%m.%Y"))
-#
-# dc.export_data("test_file.csv")
+ds = Services()
+ds.import_data("csv_files/services.csv")
+for k in ds.dict_services.keys():
+    print(k, ds.dict_services[k])
+print("------------------------")
+df = Tariffs()
+df.import_data("csv_files/tariffs.csv")
+for k in df.dict_tariffs.keys():
+    for n in df.dict_tariffs[k].services:
+        df.dict_tariffs[k].price += ds.dict_services[n].price
+    print(k, df.dict_tariffs[k])
+print("------------------------")
+dc = Clients()
+dc.import_data("csv_files/clients.csv")
+for k in dc.dict_clients.keys():
+    print(k, dc.dict_clients[k].name, dc.dict_clients[k].surname, dc.dict_clients[k].date.strftime("%d.%m.%Y"))
+
+dc.export_data("test_file.csv")
 
 ui.tableWidget.setColumnWidth(0, 200)
 ui.tableWidget.setColumnWidth(1, 100)
